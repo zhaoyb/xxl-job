032342fcac8ee20cfc2d95ccd4132cddad68883b
zhaoyabin <zhao_yabin@sohu.com>
2020-07-20 17:19:55 +0800

    add docs

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/route/ExecutorRouteStrategyEnum.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/route/ExecutorRouteStrategyEnum.java
@@ -1,6 +1,14 @@
 package com.xxl.job.admin.core.route;
 
-import com.xxl.job.admin.core.route.strategy.*;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteBusyover;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteConsistentHash;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteFailover;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteFirst;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteLFU;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteLRU;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteLast;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteRandom;
+import com.xxl.job.admin.core.route.strategy.ExecutorRouteRound;
 import com.xxl.job.admin.core.util.I18nUtil;
 
 /**
@@ -8,15 +16,45 @@ import com.xxl.job.admin.core.util.I18nUtil;
  */
 public enum ExecutorRouteStrategyEnum {
 
+    /**
+     * 第一个
+     */
     FIRST(I18nUtil.getString("jobconf_route_first"), new ExecutorRouteFirst()),
+    /**
+     * 最后一个
+     */
     LAST(I18nUtil.getString("jobconf_route_last"), new ExecutorRouteLast()),
+    /**
+     * 轮询
+     */
     ROUND(I18nUtil.getString("jobconf_route_round"), new ExecutorRouteRound()),
+    /**
+     * 随机
+     */
     RANDOM(I18nUtil.getString("jobconf_route_random"), new ExecutorRouteRandom()),
+    /**
+     * 一致性HASH
+     */
     CONSISTENT_HASH(I18nUtil.getString("jobconf_route_consistenthash"), new ExecutorRouteConsistentHash()),
+    /**
+     * 最不经常使用
+     */
     LEAST_FREQUENTLY_USED(I18nUtil.getString("jobconf_route_lfu"), new ExecutorRouteLFU()),
+    /**
+     * 最近最久未使用
+     */
     LEAST_RECENTLY_USED(I18nUtil.getString("jobconf_route_lru"), new ExecutorRouteLRU()),
+    /**
+     * 故障转移
+     */
     FAILOVER(I18nUtil.getString("jobconf_route_failover"), new ExecutorRouteFailover()),
+    /**
+     * 忙碌转移
+     */
     BUSYOVER(I18nUtil.getString("jobconf_route_busyover"), new ExecutorRouteBusyover()),
+    /**
+     * 分片广播
+     */
     SHARDING_BROADCAST(I18nUtil.getString("jobconf_route_shard"), null);
 
     ExecutorRouteStrategyEnum(String title, ExecutorRouter router) {
@@ -30,13 +68,14 @@ public enum ExecutorRouteStrategyEnum {
     public String getTitle() {
         return title;
     }
+
     public ExecutorRouter getRouter() {
         return router;
     }
 
-    public static ExecutorRouteStrategyEnum match(String name, ExecutorRouteStrategyEnum defaultItem){
+    public static ExecutorRouteStrategyEnum match(String name, ExecutorRouteStrategyEnum defaultItem) {
         if (name != null) {
-            for (ExecutorRouteStrategyEnum item: ExecutorRouteStrategyEnum.values()) {
+            for (ExecutorRouteStrategyEnum item : ExecutorRouteStrategyEnum.values()) {
                 if (item.name().equals(name)) {
                     return item;
                 }
